7d7e3f8d352b110400630237e2d606d0bdfa22ed
choper <kasidit.suwa@gmail.com>
2026-06-08 15:34:43 +0700

    feat(tsconfig): add Jest types for improved testing support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'node:path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
@@ -10,7 +9,6 @@ import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: join(__dirname, '.env'),
       isGlobal: true,
     }),
     PrismaModule,
